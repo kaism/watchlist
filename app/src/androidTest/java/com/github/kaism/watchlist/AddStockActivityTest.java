8b29ff55c31d446e59f83af39038c6bd1407dcb2
krystal <kaism@pm.me>
2020-08-11 17:47:54 +0200

    Add add stock activity

--- app/src/androidTest/java/com/github/kaism/watchlist/AddStockActivityTest.java
+++ app/src/androidTest/java/com/github/kaism/watchlist/AddStockActivityTest.java
@@ -1,13 +1,13 @@
 package com.github.kaism.watchlist;
 
 
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
@@ -19,29 +19,13 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class MainActivityTest {
+public class AddStockActivityTest {
 
 	@Rule
-	public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+	public ActivityTestRule<AddStockActivity> activityRule = new ActivityTestRule<>(AddStockActivity.class);
 
 	@Test
-	public void app_opens() {
-		onView(withText(R.string.app_name)).check(matches(isDisplayed()));
-	}
-
-	@Test
-	public void get_started_instructions_displays_if_empty() {
-		onView(withText(R.string.empty_text)).check(matches(isDisplayed()));
-	}
-
-	@Test
-	public void button_to_add_is_displayed() {
-		onView(withId(R.id.fab_add)).check(matches(isDisplayed()));
-	}
-
-	@Test
-	public void button_opens_add_activity() {
-		onView(withId(R.id.fab_add)).perform(click());
+	public void activity_opens() {
 		onView(withText(R.string.add_stock)).check(matches(isDisplayed()));
 	}
 
