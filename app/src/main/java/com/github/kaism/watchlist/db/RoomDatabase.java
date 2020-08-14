065e18301fc5a148948a3aa3ec93e5ce36eb876b
Krystal <kaism@pm.me>
2020-08-14 07:41:33 +0200

    Add database

---
+++ app/src/main/java/com/github/kaism/watchlist/db/RoomDatabase.java
@@ -0,0 +1,25 @@
+package com.github.kaism.watchlist.db;
+
+import android.content.Context;
+import androidx.room.Database;
+import androidx.room.Room;
+
+
+@Database(entities = {}, version = 1, exportSchema = false)
+public abstract class RoomDatabase extends androidx.room.RoomDatabase {
+	private static RoomDatabase INSTANCE;
+
+	public static RoomDatabase getDatabase(final Context context) {
+		if (INSTANCE == null) {
+			synchronized (RoomDatabase.class) {
+				if (INSTANCE == null) {
+					INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+							RoomDatabase.class, "kaism.watchlist.db")
+							.build();
+				}
+			}
+		}
+		return INSTANCE;
+	}
+
+}
