fcafb5c3f6877a5c76449ae681ecaea7c9053001
krystal <kaism@pm.me>
2020-10-27 09:16:04 +0100

    Add repository test

---
+++ app/src/androidTest/java/com/github/kaism/watchlist/db/StockRepositoryTest.java
@@ -0,0 +1,68 @@
+package com.github.kaism.watchlist.db;
+
+import android.os.SystemClock;
+
+import androidx.test.core.app.ActivityScenario;
+
+import com.github.kaism.watchlist.MainActivity;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
+
+public class StockRepositoryTest {
+	public StockRepository stockRepository;
+	private Stock stock = new Stock("AMD");
+
+	@Before
+	public void getRepository() {
+		ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
+		scenario.onActivity(new ActivityScenario.ActivityAction<MainActivity>() {
+			@Override
+			public void perform(MainActivity activity) {
+				stockRepository = new StockRepository(activity.getApplication());
+			}
+		});
+		assertThat(stockRepository, not(equalTo(null)));
+	}
+
+	@Test
+	public void canSaveStock() {
+		Stock result;
+		String symbol = stock.getSymbol();
+
+		// insert
+		stockRepository.insert(stock);
+		SystemClock.sleep(500); // give it time to do the async task
+
+		// verify in database
+		result = stockRepository.getStockBySymbol(symbol);
+		assertThat(result, not(equalTo(null)));
+	}
+
+	@Test
+	public void canDeleteStock() {
+		Stock result;
+		String symbol = stock.getSymbol();
+
+		// insert
+		stockRepository.insert(stock);
+		SystemClock.sleep(500); // give it time to do the async task
+
+		// verify in database
+		result = stockRepository.getStockBySymbol(symbol);
+		assertThat(result, not(equalTo(null)));
+
+		// delete
+		stockRepository.delete(stock);
+		SystemClock.sleep(500); // give it time to do the async task
+
+		// verify not in database
+		result = stockRepository.getStockBySymbol(symbol);
+		assertThat(result, equalTo(null));
+	}
+
+}
---
+++ app/src/androidTest/java/com/github/kaism/watchlist/ui/TestHelper.java
@@ -0,0 +1,44 @@
+package com.github.kaism.watchlist.ui;
+
+import androidx.test.core.app.ActivityScenario;
+
+import com.github.kaism.watchlist.MainActivity;
+import com.github.kaism.watchlist.db.Stock;
+import com.github.kaism.watchlist.db.StockRepository;
+
+import java.util.List;
+
+public class TestHelper {
+	private static StockRepository stockRepository;
+
+	public static StockRepository getRepository() {
+		ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
+		scenario.onActivity(new ActivityScenario.ActivityAction<MainActivity>() {
+			@Override
+			public void perform(MainActivity activity) {
+				stockRepository = new StockRepository(activity.getApplication());
+			}
+		});
+		return stockRepository;
+	}
+
+	public static void deleteStocks(StockRepository stockRepository, List<Stock> stocks) {
+		if (stocks.size() > 0) {
+			for (Stock stock : stocks) {
+				stockRepository.delete(stock);
+			}
+		}
+	}
+
+	public static void addStocks(StockRepository stockRepository, List<Stock> stocks) {
+		if (stocks.size() > 0) {
+			for (Stock stock : stocks) {
+				stockRepository.insert(stock);
+			}
+		}
+
+	}
+
+
+
+}
