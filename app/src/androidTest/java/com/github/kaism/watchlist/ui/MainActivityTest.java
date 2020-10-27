b1b335aa5c5676c7f5520875378da014c6a11a9d
krystal <kaism@pm.me>
2020-10-27 09:15:32 +0100

    Split main activity test

---
+++ app/src/androidTest/java/com/github/kaism/watchlist/ui/MainActivityTest.java
@@ -0,0 +1,41 @@
+package com.github.kaism.watchlist.ui;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import com.github.kaism.watchlist.MainActivity;
+import com.github.kaism.watchlist.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+public class MainActivityTest {
+	@Rule
+	public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+
+	@Test
+	public void verifyDisplay() {
+		// verify app name is displayed
+		onView(withText(R.string.app_name)).check(matches(isDisplayed()));
+
+		// verify empty text is displayed
+		onView(withText(R.string.empty_text)).check(matches(isDisplayed()));
+
+		// verify button to add a stock is displayed
+		onView(withId(R.id.button_add_stock)).check(matches(isDisplayed()));
+	}
+
+	@Test
+	public void verifyNavigation() {
+		// verify clicking on add button opens add stock activity
+		onView(withId(R.id.button_add_stock)).perform(click());
+		onView(withText(R.string.add_stock_activity_title)).check(matches(isDisplayed()));
+	}
+
+}
---
+++ app/src/androidTest/java/com/github/kaism/watchlist/ui/StockListTest.java
@@ -0,0 +1,75 @@
+package com.github.kaism.watchlist.ui;
+
+import com.github.kaism.watchlist.R;
+import com.github.kaism.watchlist.db.Stock;
+import com.github.kaism.watchlist.db.StockRepository;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.not;
+
+public class StockListTest {
+	private StockRepository stockRepository;
+	private List<Stock> backupStocks;
+
+	@Before
+	public void setUp() {
+		stockRepository = TestHelper.getRepository();
+		backupStocks = stockRepository.getStocks();
+		TestHelper.deleteStocks(stockRepository, backupStocks);
+	}
+
+	@After
+	public void restoreDatabase() {
+		TestHelper.deleteStocks(stockRepository, stockRepository.getStocks());
+		TestHelper.addStocks(stockRepository, backupStocks);
+	}
+
+	@Test
+	public void verifyEmptyDisplay() {
+		// verify empty text is displayed
+		onView(withId(R.id.empty_text)).check(matches(isDisplayed()));
+
+		// verify button to add a stock is displayed
+		onView(withId(R.id.button_add_stock)).check(matches(isDisplayed()));
+	}
+
+	@Test
+	public void verifyOneStockDisplay() {
+		// add stock
+		stockRepository.insert(new Stock("AMD"));
+
+		// verify empty text not displayed
+		onView(withId(R.id.empty_text)).check(matches(not(isDisplayed())));
+
+		// verify button to add a stock is displayed
+		onView(withId(R.id.button_add_stock)).check(matches(isDisplayed()));
+	}
+
+	@Test
+	public void verifyMultipleStockDisplay() {
+		// add stocks
+		String[] symbols = {
+				"AAPL", "AMD", "AMXN", "ATVI", "BABA", "CRM", "EA", "FB", "GOOG", "INTC", "MSFT", "MTCH",
+				"NVDA", "PYPL", "TIVO", "TSLA", "TTD", "YELP", "ZG"
+		};
+		for (String symbol : symbols) {
+			stockRepository.insert(new Stock(symbol));
+		}
+
+		// verify empty text not displayed
+		onView(withId(R.id.empty_text)).check(matches(not(isDisplayed())));
+
+		// verify button to add a stock is displayed
+		onView(withId(R.id.button_add_stock)).check(matches(isDisplayed()));
+	}
+
+}
